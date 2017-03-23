845b28fb2dbe426ffb187e3f2078bab38c7316f5
6iruk <biruktarega@gmail.com>
2017-03-23 23:46:08 +0300

    Edit url in render

--- push_page/views.py
+++ push_page/views.py
@@ -7,4 +7,4 @@ def Push_Page(request,quick_page):
     archives = PushPage.objects.all()
     page = PushPage.objects.get(id=quick_page)
     context = {'page':page,'archives':archives}
-    return render(request,'main/Push_Page.html',context)
+    return render(request,'push_page/Push_Page.html',context)
